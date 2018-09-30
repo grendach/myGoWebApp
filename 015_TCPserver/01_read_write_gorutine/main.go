78cc5e580f276931d0d71b3da8d37e79bef28124
grendach <dmytro.grendach@nokia.com>
2018-09-30 18:56:00 +0200

    add r13 encrypt

---
+++ 015_TCPserver/01_read_write_gorutine/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Panicln(err)
+			continue
+		}
+		go handle(conn)
+
+	}
+}
+
+func handle(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		log.Fatalln("Connection timeout")
+
+	}
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I can hear you: %s\n", ln)
+
+	}
+	defer conn.Close()
+	// Below code never will be executed
+	fmt.Println("!!! Code got here!!!")
+}
